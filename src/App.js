da26c61cf20a1302d868fa3f407b4287b06b4b27
Zhibo Wang <zhibo.wang1@gmail.com>
2018-02-16 02:09:25 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+const tarot_card_data = require('./data/tarot_card_assets');
+
+var replace_text_random = function(orig_text, replace_word_list, pct_replace){
+	var orig_text_array = orig_text.split(' ')
+	var i
+	var orig_text_rand_index
+	var replace_word_rand_index
+	var max_replace_words = Math.floor(orig_text_array.length * pct_replace)
+
+	for (i = 0; i < max_replace_words; i++) {
+		orig_text_rand_index = Math.floor(Math.random() * orig_text_array.length)
+		replace_word_rand_index = Math.floor(Math.random() * replace_word_list.length)
+
+    	orig_text_array[orig_text_rand_index] = replace_word_list[replace_word_rand_index]
+	}
+
+	return orig_text_array.join(' ')
+
+}
+
+class TarotCard extends Component {
+	constructor(props){
+		super(props)
+		console.log(this.props)
+	}
+
+	render() {
+		return (
+			<div className="TarotCard">
+				<img src = {this.props.image_path}></img>
+			</div>
+		)
+	}
+}
+
+class TarotCardDescription extends Component {
+	constructor(props){
+		super(props)
+	}
+
+	render() {
+		return (
+			<div className="TarotCardDescription">
+				{this.props.card_description}>
+			</div>
+		)
+	}
+}
+
+class App extends Component {
+
+  	render() {
+		var randomCardIndex = Math.floor(Math.random() * tarot_card_data.length)
+    return (
+		<div>
+			<TarotCard image_path = {'./imgs/' + tarot_card_data[randomCardIndex].image_path} />
+			<TarotCardDescription card_description = {replace_text_random(tarot_card_data[randomCardIndex].description, ['farts'], 0.5)} />
+		</div>
+    );
+  }
+}
+
+export default App;
---
+++ src/data/tarot_card_assets.js
@@ -0,0 +1,121 @@
+var card_data =
+	[
+		{"image_path":"343px-RWS_Tarot_00_Fool.jpg",
+		"description": `Pamela Coleman-Smith's artful rendition of The Fool in the Rider-Waite Tarot deck is often used to represent Tarot in general. Early classical versions of The Fool card, however, portray quite a different character -- a person driven by base needs and urges, who has fallen into a state of poverty and deprivation.
+
+In some earlier instances, The Fool is made out to be a carnival entertainer or a huckster. In others, he is portrayed as decrepit and vulnerable -- as the cumulative result of his delusions and failures. Not until the 20th century do you see the popular Rider-Waite image of The Fool arise -- that of an innocent soul before its fall into matter, untainted by contact with society and all its ills.
+
+Modern decks usually borrow from the Rider-Waite imagery. Most Fool cards copy the bucolic mountainside scene, the butterfly, and the potential misplaced step that will send The Fool tumbling into the unknown. Don't forget, though, that the earlier versions of this card represented already-fallen humanity, over-identified with the material plane of existence, and beginning a pilgrimage toward self-knowledge and, eventually, wisdom. The Fool reminds us to recognize the path of personal development within ourselves -- and the stage upon that path where we find ourselves -- in order to energize our movement toward deeper self-realization.
+
+In the Advice Position
+Let go of preconceived ideas and remain open to change.
+
+The card in the Advice position suggests a course of action which will harmonize what you want with what is currently possible.
+
+The Fool advises that you lighten up. Let yourself be spontaneous enough to stretch beyond the realm of logic. There is no advantage to be gained by thinking you possess the knowledge, power, or control to direct reality. Open and receive without question, instead of trying to manage what's happening right now. The Fool has no ambition to manipulate a specific outcome. Just be happy to be part of the whole.
+
+Release any demands or expectations. Give your complete attention to events as they are occurring in the present moment.`
+		},
+		{"image_path":"345px-RWS_Tarot_02_High_Priestess.jpg",
+		"description": `The High Priestess is a Major Arcana, or "trump" card, that represents human wisdom. The High Priestess can be viewed as a kind of female Pope (a Papess), or the ancient Egyptian Priestess of Isis, the even more ancient snake and bird goddesses, the Greek goddess Persephone, or Eve, before the fall.
+
+For the accused heretics who were burnt at the stake for revering her in the 14th and 15th century, the Priestess symbolized the prophecy of the return of the Holy Spirit, which was perceived as the female aspect of the Holy Trinity.
+
+In terms of the Major Arcana ordering of cards, The High Priestess appears in the sequence as soon as the Fool decides he wants to develop his innate powers, making a move toward becoming a Magician. The High Priestess is his first teacher, representing the inner life and the method for contacting it, as well as the contemplative study of nature and spiritual mystery.
+
+In the Advice Position
+Put self-cultivation at the top of your daily priority list.
+
+The card in the Advice position suggests a course of action which will harmonize what you want with what is currently possible.
+
+The High Priestess advises you to adhere to your chosen spiritual practice on a more regular basis. If you want the benefits of evolution, you'll have to cooperate with spirit. We all have distractions, demands -- a whole life full of reasons why we cannot find the time to retreat into our inner sanctum.
+
+Until you consider this as vital to your well-being as the need to eat and sleep, you are likely to be eternally restless and deeply dissatisfied. A spiritual routine that suits your temperament, practiced every day, is the most trustworthy path to freedom.`
+		},
+		{"image_path":"RWS_Tarot_01_Magician.jpg",
+		"description":`Traditionally, The Magician is one who can demonstrate hands-on magic -- as in healing, transformative rituals, alchemical transmutations, charging of talismans, and the like. A modern Magician is any person who completes the circuit between heaven and Earth; one who seeks to bring forth the divine gold within her or himself.
+
+At the birth of Tarot, even a gifted healer who was not an ordained clergyman was considered to be in league with The Devil! For obvious reasons, the line between fooling the eye with sleight of hand and charging the world with magical will, was not clearly differentiated in the early Tarot cards.
+
+Waite's image of The Magician as the solitary ritualist communing with the spirits of the elements -- with its formal arrangement of symbols -- is a token of the freedom we have in modern times to declare our spiritual politics without fear of reprisal. The older cards were never so explicit about what The Magician was doing. It's best to keep your imagination open with this card. Visualize yourself manifesting something unique, guided by evolutionary forces that emerge spontaneously from within your soul.
+
+In the Advice Position
+Trust your inspiration. You are smarter than you think.
+
+The card in the Advice position suggests a course of action which will harmonize what you want with what is currently possible.
+
+Have faith in your innate creativity. The Magician advises you give your questioning nature and free-associating mind plenty of room to explore the subject at hand. Behave just as if you were an open-minded and curious scientist. Through this process, you may bring freshness and clarity into the situation that is both stimulating and catalytic.
+
+You do not have to understand it all intellectually. Besides, intuition is your ace in the hole. Respond in a spontaneous manner to what is right in front of you. There is no reason to hold yourself back. Your natural urges are exactly what is needed, and, your ingenuous timing and elegant style will help smooth over any awkwardness.`
+		},
+		{"image_path":"RWS_Tarot_03_Empress.jpg",
+		"description": `The Empress is a Major Arcana, or "trump" card, that portrays the energy of the great mother. She is nature, around us but also within us, the ever-unfolding source of life-giving power. The Empress is often pictured as a pre-Christian Goddess, as the one whom The High Priestess is channeling down to Earth for the rest of us.
+
+In medieval Europe, The Empress card was painted to represent whatever queen currently ruled the land, probably to satisfy the Inquisitors. But the scholars of the Renaissance and beyond had no doubt of her true identity -- she could not be fully revealed on Tarot cards as "the woman clothed with the sun" until after the French Revolution.
+
+This supreme archetype of femininity also symbolizes fertility. It is The Empress who provides us nourishment and security. She is also sometimes seen as delighting us with flowers and fruit. A potentially terrifying aspect of this archetype manifests itself whenever karmic mood swings wipe out our plans, like a storm that has come upon us. Whatever happens, The Empress is the source of our embodiment and of natural lLaw. She might even be called "The Great Recycler."
+
+In the Advice Position
+Rather than being tough on yourself for not measuring up, know that your positive influence has facilitated favorable outcomes.
+
+The card in the Advice position suggests a course of action which will harmonize what you want with what is currently possible.
+
+The Empress advises you to trust in the good sense you have shown up to this point. Recognize your good intentions in carrying out your responsibilities as a compassionate human. You are capable of demonstrating the finest aspects of your personality.
+
+Demonstrate this through caring actions, a forgiving and generous attitude, and wise understanding of others' needs and struggles. Bring a healing influence to the current situation and take full credit for the supportive part you play.
+
+`
+		},
+		{"image_path":"RWS_Tarot_04_Emperor.jpg",
+		"description":`In the most practical terms, The Emperor Tarot card represents the highest leadership, a head of state, or the most exemplary and powerful person in the realm. This archetypal ruler is responsible for the affairs of a society or community, which are directly proportional to his well being and happiness.
+
+The more enlightenment and cosmic perspective this energy brings, the better life is for all. The Emperor archetype masters the world of matter and physical manifestation. When you apply this card to your situation, acknowledge your potentials for mastery. Reinforce a sense of sovereignty within yourself, despite any self-limiting beliefs, habits, or appearances to the contrary.
+
+In the Advice Position
+Draw upon the capable inner resources you possess to get the task completed.
+
+The card in the Advice position suggests a course of action which will harmonize what you want with what is currently possible.
+
+When the Emperor arises, he advises you get in touch with your inner sovereignty and natural self-possession. Realize that somewhere deep inside you is the memory of royalty. Reach within to find that strain of natural nobility and leadership. These innate qualities will help you manage your current situation.
+
+The Emperor suggests that you have the necessary abilities to be the final authority. This situation is an opportunity to showcase your competence and skill. Act with the confidence of someone who knows how to take care of business.`
+
+		},
+		{"image_path":"RWS_Tarot_05_Hierophant.jpg",
+		"description":`The Hierophant Tarot this card refers to a master and the learning of practical lessons from the study of natural law. This energy of this card points to some agent or resource that can reveal the secrets of life, the cycles of the Moon and tides, the links between human beings and the heavens.
+
+Because monasteries were the only places a person could learn to read and write in the Middle Ages, a Hierophant was one to whom a student would petition for entry. He was the one to set the curriculum for the neophyte's course of study.
+
+Often pictured with the right hand raised in blessing, the Hierophant is linked with the ancient lineage of Melchezidek, initiator of the Hebrew priestly tradition, the one who passes on the teachings. All shamans of any tradition draw upon this archetype.
+
+In the Advice Position
+Develop your expertise, and have faith that you are a master in the making.
+
+The card in the Advice position suggests a course of action which will harmonize what you want with what is currently possible.
+
+The Hierophant advises that you return to the role of a meticulous student. Learn everything you can about your chosen area. Let that knowledge become a part of you and an operative influence on your day-to-day awareness. In this way, you can slowly and steadily establish real credibility in your field or chosen subject.
+
+Earn respect and recognition by completing your education and broadening your experience. If you already have all the necessary experience you need, then rewrite your resume so others can appreciate who you are and what you can bring to a situation. Focus on your goal and be determined. You may be destined to be a master in your realm.
+		`
+
+		},
+		{"image_path":"RWS_Tarot_06_Lovers.jpg",
+		"description":`Although it has taken on a strictly romantic revision of meaning in some modern decks, traditionally The Lovers Tarot card reflected the challenges of choosing a partner. At a crossroads, one cannot take both paths. The images on this card in different decks have varied more than most, because we have had so many ways of looking at sex and relationships across cultures and centuries.
+
+Classically, the energy of this card reminded us of the real challenges posed by romantic relationships, with the protagonist often shown in the act of making an either-or choice. To partake of a higher ideal often requires sacrificing the lesser option. The path of pleasure eventually leads to distraction from spiritual growth. The gratification of the personality eventually gives way to a call from spirit as the soul matures.
+
+Modern decks tend to portray the feeling of romantic love with this card, showing Adam and Eve at the gates of Eden when everything was still perfect. This interpretation portrays humanity before the fall, and can be thought to imply a different sort of choice -- the choice of evolution over perfection, or the choice of personal growth through relationship -- instead of a fantasy where everything falls into place perfectly and is taken care of without effort.
+
+In the Advice Position
+Creative compromise helps you accept your commitments fully.
+
+The card in the Advice position suggests a course of action which will harmonize what you want with what is currently possible.
+
+The Lovers card advises that you study your options and make the wisest choice. Carefully consider your long-term interests. There is no judgment on what you choose to keep from the array of possibilities before you. Just watch out for choices that will produce dissatisfaction and discontent.
+
+Be willing to make some compromises, then stick with the commitments you finally make. Trust your intuition along with your rational intellect, and once you make your choice, carry it out with conviction.`
+		}
+	]
+
+
+module.exports = card_data
